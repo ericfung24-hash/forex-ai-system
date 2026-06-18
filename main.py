08c359b81b68c849b8826482fb653ca6a44d7e39
ericfung24-hash <ericfung24@gmail.com>
2026-06-18 09:28:56 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from utils.loader import load_data
+from features.indicators import add_indicators
+from labeling.atr_label import create_labels
+from models.xgb_model import train_xgb
+
+df = load_data('data/raw/EURUSD_H1.csv')
+
+df = add_indicators(df)
+df = create_labels(df)
+
+features = ['rsi', 'macd', 'atr']
+X = df[features]
+y = df['label']
+
+model = train_xgb(X, y)
+
+df['prob'] = model.predict_proba(X)[:,1]
