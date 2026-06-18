accc1ab4021dae840b1696ec7a7ff6cf7eeb0121
ericfung24-hash <ericfung24@gmail.com>
2026-06-18 12:41:31 +0800

    Update data_loader.py

--- app/data_loader.py
+++ app/data_loader.py
@@ -3,14 +3,28 @@ import os
 
 DATA_DIR = "data"
 
+def read_csv_safe(path):
+
+    try:
+        return pd.read_csv(path, encoding='utf-8')
+    except:
+        try:
+            return pd.read_csv(path, encoding='utf-16')
+        except:
+            return pd.read_csv(path, encoding='latin1')
+
 def load_symbol(symbol, tf="H1"):
     for f in os.listdir(DATA_DIR):
         if symbol in f and tf in f:
             path = os.path.join(DATA_DIR, f)
-            df = pd.read_csv(path)
+
+            df = read_csv_safe(path)
+
             df['timestamp'] = pd.to_datetime(df['timestamp'])
             df = df.sort_values('timestamp').set_index('timestamp')
+
             return df
+
     raise Exception(f"{symbol} not found")
 
 def load_all():
@@ -19,7 +33,9 @@ def load_all():
         "US30","GER40","JPN225",
         "UK100","EUSTX50","XAUUSD","VIX"
     ]
+
     data = {}
     for sym in symbols:
         data[sym] = load_symbol(sym)
+
     return data
