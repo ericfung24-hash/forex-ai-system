254433a9f6e8a059dadebd35dc94f49013652d36
ericfung24-hash <ericfung24@gmail.com>
2026-06-18 12:49:06 +0800

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 def load_data(path):
-    df = pd.read_csv(path)
+
+    try:
+        df = pd.read_csv(path, encoding='utf-8')
+    except:
+        try:
+            df = pd.read_csv(path, encoding='utf-16')
+        except:
+            df = pd.read_csv(path, encoding='latin1')
+
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     df = df.sort_values('timestamp').reset_index(drop=True)
+
     return df
